6c33816b05f24d5ba63bf9cdcaf0d4de6ecb8fc7
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-04-03 16:36:10 +0100

    d

--- signup.php
+++ signup.php
@@ -2,44 +2,20 @@
 $firstname = $_POST['fristname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$pass = $_POST['pass'];
 
-if (!empty($firstname) || !empty($lastname) || !empty($email) || !empty($password)) {
-    $host = "localhost";
-    $dbUsername = "Q14980258";
-    $dbPassword = "aetoorau";
-    $dbname = "Q14980258";   
-
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-    if(mysqli_connect_error()) {
-        die('Connect Error('. mysqli_connect_errno().')' . mysqli_connect_error());
+    $conn = new mysqli('localhost', 'Q14980258', 'aetoorau', 'Q14980258');
+    if($conn->connect_error) {
+        die('Connection Failed :'.$conn->connect_error);
     }
     else {
-        $select = "SELECT email From signup Where email = ? Limit 1";
-        $insert = "INSERT Into signup (firstname, lastname, email, password) values (?, ?, ?, ?)";
-        
-        $stmt = $conn-> prepare($select);
-        $stmt->bind_param("s", $email);
-        $stmt-> execute();
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $colnum = $stmt->num_column;
-
-        if($colnum == 0) {
-            $stmt = $conn-> prepare($insert);
-            $stmt-> bind_param("ssss", $firstname, $lastname, $email, $password);
-            $stmt-> execute();
-            header('user.html');      
-        }
-        else {
-            echo "Email is already registered";
-        }
+        $stmt = $conn->prepare("insert into signup (firstname, lastname, email, pass)
+         values(?, ?, ?, ?)");
+    
+        $stmt->bind_param('ssss', $firstname, $lastname, $email, $pass);
+        $stmt->execute();
+        echo "Data entered successfully";
         $stmt->close();
-        $conn->close();       
+        $conn->close();    
     }
-}
-else {
-    echo "All fiels are required";
-    die();
-}
 ?>
