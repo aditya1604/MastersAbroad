d375645f977fedb3c5eb44d36b13eb3e7c99bb54
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-04-06 14:10:59 +0100

    done

--- signup.php
+++ signup.php
@@ -1,20 +1,21 @@
 <?php
-$firstname = $_POST['fristname'];
+$firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 
-    $conn = new mysqli('localhost', 'Q14980258', 'aetoorau', 'Q14980258');
+    $conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
     if($conn->connect_error) {
         die('Connection Failed :'.$conn->connect_error);
     }
     else {
         $stmt = $conn->prepare("insert into signup (firstname, lastname, email, pass)
          values(?, ?, ?, ?)");
-        
+
         $stmt->execute(['ssss', $firstname, $lastname, $email, $pass]);
         echo "Data entered successfully";
         $stmt->close();
         $conn->close();    
     }
+    header("Location: user.html");
 ?>
