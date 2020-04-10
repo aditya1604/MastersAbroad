586dbdf744d4a2e3124ed7588b163838149b08ee
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-04-10 12:45:22 +0100

    done

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+    
+    $conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
+
+    session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($conn,$_POST['email']);
+      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      
+      $sql = "SELECT FROM signup WHERE email = '$myusername' and pass = '$mypassword'";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: user.html");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
