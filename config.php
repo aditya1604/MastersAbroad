b2acd3f59433a3df9927e4a089b97cf5f0518d08
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-04-29 18:30:49 +0100

    chanegs done

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'Q14980258');
+   define('DB_PASSWORD', 'aetoorau');
+   define('DB_DATABASE', 'Q14980258');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
--- login.php
+++ login.php
@@ -1,51 +1,27 @@
 <?php
- 
+
+// Grab User submitted information
+$email = $_POST["email"];
+$pass = $_POST["pass"];
+echo "$email $pass";
+// Connect to the database
 $conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
 
-session_start();
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  // username and password sent from form 
-  
-  $myusername = mysqli_real_escape_string($conn,$_POST['email']);
-  $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
-  
-  $sql = "SELECT FROM signup WHERE email = '$myusername' and pass = '$mypassword'";
-  $result = mysqli_query($conn,$sql);
-  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-  $active = $row['active'];
-  
-  $count = mysqli_num_rows($result);
-  
-  // If result matched $myusername and $mypassword, table row must be 1 row
+$result = "SELECT email, pass FROM signup WHERE email = $email and pass = $pass";
+$stmt = $conn->prepare($result);
+$stmt->bindValue(':email', $email);
+$stmt->bindValue(':pass', $pass);
     
-  if($count == 1) {
-     session_register("myusername");
-     $_SESSION['login_user'] = $myusername;
-     
-     header("location: user.html");
-  }else {
-     $error = "Your Login Name or Password is invalid";
-     header("location: login.html");
-  }
-}
+    $stmt->execute();
 
+       $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+   if($row["email"]==$email && $row["pass"]==$pass) {
+   header("Location: user.html");
+   }
+   else {
+    echo"Sorry, your credentials are not valid, Please try again.";
+   }
 ?>
 <?php
-/*
-$username = $_POST["email"];
-$password = $_POST["pass"];
-if ($username != "" && $password != "") {
-require_once("database.php");
-$sql = "SELECT email FROM signup
- WHERE email='$username'
- AND pass='$password'";
-$result = $db->query($sql);
-if ($result->rowCount() > 0) {
-session_start();
-$_SESSION["uid"] = $result["email"];
-header("location: user.html");
-}
-}
-?>
-*/
