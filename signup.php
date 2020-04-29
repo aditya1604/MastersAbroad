50da74f8fa6503010a8cca99e1d0e2ea150ed802
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-04-29 13:04:23 +0100

    basic change done

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
-$pass = $_POST['pass'];
+$password = $_POST['pass'];
     echo "<h1>$firstname $lastname $email $pass</h1>";
     $conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
     if($conn->connect_error) {
@@ -12,7 +12,7 @@ $pass = $_POST['pass'];
         $stmt = $conn->prepare("insert into signup (firstname, lastname, email, pass)
          values(?, ?, ?, ?)");
 
-        $stmt->execute([$firstname, $lastname, $email, $pass]);
+        $stmt->execute([$firstname, $lastname, $email, $password]);
         header("Location:user.html");
        
     }
