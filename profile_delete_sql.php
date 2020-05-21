90a1e807353e8ec5c66ba1eee634180d24ea7ffb
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-05-21 08:53:43 +0100

    done

--- profile_delete_sql.php
+++ profile_delete_sql.php
@@ -3,13 +3,12 @@ $email = $_POST['email'];
 
 try {
     $conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // sql to delete a record
     $sql = "DELETE FROM signup WHERE email= $email";
 
-    // use exec() because no results are returned
+    
     $conn->exec($sql);
     echo "Record deleted successfully";
     }
