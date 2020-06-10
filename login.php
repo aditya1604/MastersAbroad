21bde72d248d7a7c1b685dd2c336704daae89634
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-06-10 17:31:25 +0100

    chanegs

--- login.php
+++ login.php
@@ -1,26 +1,20 @@
+
 <?php
-
-
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-
-
-$conn = new PDO("mysql:host=localhost;dbname=Q14980258;", "Q14980258", "aetoorau");
-
-
-$result = "SELECT email, pass FROM signup WHERE email = $email and pass = $pass";
-$stmt = $conn->prepare($result);
-
-    
-    $stmt->execute([$email, $pass]);
-
-       $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-   if($row["email"]==$email && $row["pass"]==$pass) {
-   header("Location: user.html");
-   }
-   else {
+$username = $_POST["email"];
+$password = $_POST["pass"];
+if ($username != "" && $password != "") {
+require_once("database.php");
+$sql = "SELECT id FROM signup
+ WHERE email='$username'
+ AND pass='$password'";
+$result = $db->query($sql);
+if ($result->rowCount() > 0) {
+session_start();
+$_SESSION["uid"] = $result["id"];
+header("Location: user.html");
+}
+else {
     echo"Sorry, your credentials are not valid, Please try again.";
    }
+}
 ?>
-<?php
