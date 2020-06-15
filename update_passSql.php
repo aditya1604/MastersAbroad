f7c5ca071c9ecfbd9df239d5c5288a92c11c436a
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-06-15 01:07:47 +0100

    done

--- update_passSql.php
+++ update_passSql.php
@@ -10,13 +10,13 @@ try {
 
   $sql =  'UPDATE signup SET  pass = "' . $pass . '"  WHERE email = "' . $email . '" ';
 
-  // Prepare statement
+
   $stmt = $conn->prepare($sql);
 
-  // execute the query
+
   $stmt->execute();
 
-  // echo a message to say the UPDATE succeeded
+  
   echo $stmt->rowCount() . " records UPDATED successfully";
   session_start();
 session_destroy();
