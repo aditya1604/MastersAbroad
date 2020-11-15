0456ea1ac6798445dde788d10fe9be53a3e8a959
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-11-15 11:43:19 +0000

    r

--- search.js
+++
@@ -1,9 +0,0 @@
-// FIlter anything
-$(document).ready(function () {
-    $("#anythingSearch").on("keyup", function () {
-      var value = $(this).val().toLowerCase();
-      $("#hid").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-  });
