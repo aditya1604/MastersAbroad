c5c05232daf6fc36d6a15bc3868e316d422c3382
aditya1604 <60433095+aditya1604@users.noreply.github.com>
2020-12-02 16:37:16 +0000

    done

--- login.php
+++ login.php
@@ -1,18 +1,21 @@
 <?php
     $user = $_POST['username'];
     $pass = $_POST['password'];
-if ($user != "" && $pass != "") {
-require 'database.php';
-$sql = "SELECT userid FROM signup WHERE email='$user'
- AND pass ='$pass'";
-$result = $conn->query($sql);
-if($result->rowCount() > 0) {
-  session_start();
-  $_SESSION['username'] = $user;
-  header('location:user.php');
-} else {
-      echo "Username/Password is wrong";
-      }
-
+    
+    if ($user != "" && $pass != "") 
+    { 
+      require 'database.php';
+      $sql = "SELECT userid FROM signup WHERE email='$user'
+              AND pass ='$pass'";
+      $result = $conn->query($sql);
+      if($result->rowCount() > 0) 
+        {
+          session_start();
+          $_SESSION['username'] = $user;
+          header('location:user.php');
+        } else 
+        {
+          echo "Username/Password is wrong";
+        }
 }
 ?>
